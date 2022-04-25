5aa9a1d3c0a8f7df842fba65eb8ea1ed9bc66839
MaxLyonel <leorexx100@gmail.com>
2022-04-24 22:54:51 -0400

    Preparando entorno para agregar componente Proceedings

---
+++ resources/assets/js/components/loan/Proceedings.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card flat>
+
+    <v-card-title class="pb-0">
+      <v-toolbar>
+        Aquí ira el título
+      </v-toolbar>
+    </v-card-title>
+
+    <v-card-title> 
+      Préstamos mayores a 80.000
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Búsqueda"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+
+    <v-spacer></v-spacer>
+
+    <template>
+      <v-card-text> 
+        <v-data-table
+          :headers="cabeceras"
+          :search="search"
+          :footer-props="{ itemsPerPageOptions: [8, 15, 30] }"
+        >
+        </v-data-table>
+      </v-card-text>
+    </template>
+
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: "proceedings-index",
+  components: {
+  },
+  data() {
+    return {
+      search: '',
+      cabeceras: [
+        {
+          text: 'Código Prestamo',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+          class: ['normal', 'white--text'],
+        },
+        {
+          text: 'Monto aprobado [Bs]',
+          value: 'Monto',
+          class: ['normal', 'white--text'],
+        },
+        {
+          text: 'Nombre',
+          value: 'Nombre',
+          class: ['normal', 'white--text'],
+        },
+      ]
+    }
+  },
+}
+</script>
